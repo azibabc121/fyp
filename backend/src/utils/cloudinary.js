ffc41b0772304143862badd16af00ff2a41cba18
Azib Rasheed <mac@192.168.1.19>
2024-07-07 16:31:39 +0500

    Removed unnecessary logs

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -6,26 +6,16 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View Credentials' below to copy your API secret
 });
-console.log(
-  "cloud_name : ",
-  process.env.CLOUDINARY_CLOUD_NAME,
-  "api_key : ",
-  process.env.CLOUDINARY_API_KEY,
-  "api_secret",
-  process.env.CLOUDINARY_API_SECRET
-);
-console.log("PORT : ", process.env.PORT);
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
-    console.log("localfilepath", localFilePath);
     // Upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
     // File has been uploaded successfully
-    console.log("File is uploaded on cloudinary", response.url);
+    // console.log("File is uploaded on cloudinary", response.url);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
